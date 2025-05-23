00ee918ea00f07a3aeb13c20e27d338e034a9a11
Rajat Patil <patilrajat805@gmail.com>
2025-05-24 02:59:55 +0530

    Built Http Exception Handler for services

--- src/main/java/co/in/nextgencoder/springblueprint/service/exception/ErrorResponse.java
+++ src/main/java/co/in/nextgencoder/springblueprint/service/exception/ErrorResponse.java
@@ -3,18 +3,18 @@ package co.in.nextgencoder.springblueprint.service.exception;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 public class ErrorResponse {
     private int status;
     private String error;
     private String message;
+    private String path;
 
     public ErrorResponse(int status, String error, String message, String path) {
         this.status = status;
         this.error = error;
         this.message = message;
+        this.path = path;
     }
 }
